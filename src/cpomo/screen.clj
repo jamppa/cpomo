7a4db6067f045dc8dbe57dd4aae3b30312b3980b
Jani Arvonen <jani.arvonen@gmail.com>
2012-01-12 21:28:16 +0200

    added function which creates panel for image

--- src/cpomo/screen.clj
+++ src/cpomo/screen.clj
@@ -1,14 +1,14 @@
 (ns cpomo.screen
   (:import (java.awt Robot Rectangle Toolkit))
-  (:import (javax.swing JOptionPane)))
-
-(defn say-hello []
-  (JOptionPane/showMessageDialog
-    nil "Moro!" "Tervehdys" JOptionPane/INFORMATION_MESSAGE))
+  (:import (javax.swing JOptionPane JFrame)))
 
 (defn take-screenshot [] 
   (let [robot (Robot.)]
     (.createScreenCapture robot 
       (Rectangle. (.. Toolkit getDefaultToolkit getScreenSize)))))
 
+(defn image-panel [image x y]
+  (proxy [JFrame] []
+    (paint [g]
+      (.drawImage g image x y this))))
 
